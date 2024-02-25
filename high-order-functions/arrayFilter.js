713e320b6cae7a46e2145f8f092c2cb896bf567e
nadaMAoui <maaouinada747@gmail.com>
2024-02-25 21:09:14 +0100

    add reduce function

--- high-order-functions/arrayFilter.js
+++ high-order-functions/arrayFilter.js
@@ -9,3 +9,24 @@ function filter(array, test) {
   return passed;
 }
 console.log(filter([1, 2, 3], (odd) => odd % 2 != 0));
+//console.log(SCRIPTS.filter((s) => s.direction == "ttb"));
+
+//function mapping an array
+function map(array, transform) {
+  let mapped = [];
+  for (let element of array) {
+    mapped.push(transform(element));
+  }
+  return mapped;
+}
+console.log(map([1, 2, 3], (s) => s * 2));
+
+// reduce function
+function reduce(array, combine, start) {
+  let current = start;
+  for (let elements of array) {
+    current = combine(current, elements);
+  }
+  return current;
+}
+console.log(reduce([1, 2, 3, 4], (a, b) => a + b, 0));
