87975307b9be1b5e06e74d4e153a5537e10de0f6
nadaMAoui <maaouinada747@gmail.com>
2024-03-04 21:23:30 +0100

    >>

---
+++ Scripts/domainat writing direction.js
@@ -0,0 +1,16 @@
+//dominant writing direction
+function scriptAnalyser(text) {
+  //array of each character direction in the text
+  const scriptDirection = text.split("").map(characterScript);
+  //group the characters by direction and calculate occuraction
+  const directionCounts = countBy(scriptDirection, (direction) => direction);
+  //dtermine the dominant writing direction
+  const dominantDirection = Object.keys(directionCounts).reduce((a, b) =>
+    directionCounts[a] > directionCounts[b] ? a : b
+  );
+  return dominantDirection;
+}
+
+const result = scriptAnalyser("Hello i'm a preparatory student");
+console.log(result);
+//to be continued...
